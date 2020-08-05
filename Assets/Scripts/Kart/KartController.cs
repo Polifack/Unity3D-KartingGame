c307377d1206e82a590b080c72641932d7608599
Poli <diegoroca.ordes@gmail.com>
2020-08-05 18:40:09 +0200

    Added variable camera fov according to speed

--- Assets/Scripts/Kart/KartController.cs
+++ Assets/Scripts/Kart/KartController.cs
@@ -17,15 +17,18 @@ public class KartController : MonoBehaviour
 
     //Kart movement parameters
     public float kartWeight;
+    public float gravity;
+
     public float acceleration;
     public float brakesStrength;
-    public float gravity;
+
     public float steering;
-    
+    public float rotateSpeed;
+
     public float driftPositiveMod;
     public float driftNegativeMod;
     public float driftJumpStrength;
-    
+
     public float driftTimeFirst;
     public float driftTimeSecond;
     public float driftTimeThird;
@@ -33,6 +36,11 @@ public class KartController : MonoBehaviour
     public Color driftFirstColor;
     public Color driftSecondColor;
     public Color driftThirdColor;
+
+    public float boostStrength;
+    public float maxBoostStrength;
+
+    public float speedFovModifier;
 
     //Particles
     public KartParticles particles;
@@ -42,11 +50,12 @@ public class KartController : MonoBehaviour
     private float rotate;
     private float currentRotate;
     private float currentSpeed;
-    private bool isGrounded;
 
     private bool drift = false;
     private float driftDirection;
-    private int driftAccumulator;
+    private float driftAccumulator;
+
+    private bool isGrounded;
 
     //Input data
     private float accelerationInput; //r2
@@ -54,33 +63,41 @@ public class KartController : MonoBehaviour
     private float axisInput; //axis
 
 
-
+    //Boost
+    public void doBoost(float boostModifier)
+    {
+        Vector3 boostDirection = kartVisuals.transform.forward;
+        boostDirection = boostDirection * boostStrength * boostModifier;
+        sphere.AddForce(boostDirection, ForceMode.Impulse);
+    }
 
     //Drifting
     public void setDrifting(bool b){
+        //If we are not grounded we cant drift
+        if (!isGrounded) return;
+
         driftDirection = b ? Mathf.Sign(currentRotate):0;
 
+        //press drift
         if (b && !drift)
         {
             driftAccumulator = 0;
+            
             particles.doDriftParticles(driftDirection);
             sphere.AddForce(new Vector3(0, driftJumpStrength, 0), ForceMode.Impulse);
         }
-        if (!b)
+        //release drift
+        if (!b && drift)
         {
+            doBoost(driftAccumulator );
             driftAccumulator = 0;
+            
             particles.setParticleColor(Color.white);
             particles.stopDriftParticles();
         }
         
         drift = b;
     }
-    public bool isDrifting() {
-        driftDirection = 0;
-        kartVisuals.setDriftingDirection(0);
-        return drift; 
-    }
-
 
     //Controls
     public void setupControls()
@@ -134,26 +151,22 @@ public class KartController : MonoBehaviour
     {
         if (drift)
         {
-            driftAccumulator += 1;
+            driftAccumulator = Mathf.Clamp(driftAccumulator + 1, 0, maxBoostStrength);
 
             if (driftAccumulator == driftTimeFirst)
             {
-                Debug.Log("Changing color first!");
                 particles.setParticleColor(driftFirstColor);
             }
             if (driftAccumulator == driftTimeSecond)
             {
-                Debug.Log("Changing color second!");
                 particles.setParticleColor(driftSecondColor);
             }
             if (driftAccumulator == driftTimeThird)
             {
-                Debug.Log("Changing color third!");
                 particles.setParticleColor(driftThirdColor);
             }
         }
     }
-
 
     //Unity
     private void Start()
@@ -167,7 +180,6 @@ public class KartController : MonoBehaviour
         kartVisuals.setPosition(sphere.transform.position - new Vector3(0, 1.2f, 0));
         
         //Check for axis and compute steer rotation
-
         int steerDirection = (axisInput > 0) ? 1 : -1;
         float steerAmmount = Mathf.Abs(axisInput);
         Steer(steerDirection, steerAmmount);
@@ -176,10 +188,9 @@ public class KartController : MonoBehaviour
         speed = accelerationInput * acceleration;
         speed -= brakesInput * brakesStrength;
 
-
-         //Compute speed
-         currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
-         currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+        //Compute speed
+        currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
+        currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
 
         //Check for ground slope
         RaycastHit hit;
@@ -198,15 +209,16 @@ public class KartController : MonoBehaviour
     private void FixedUpdate()
     {
         //Apply strengths to the sphere
-        if (isGrounded)
-            sphere.AddForce(kartVisuals.GetForwardVector() * currentSpeed, ForceMode.Acceleration);
-
+        sphere.AddForce(kartVisuals.GetForwardVector() * currentSpeed, ForceMode.Acceleration);
         sphere.AddForce(Vector3.down * kartWeight * gravity, ForceMode.Acceleration);
 
         //Rotate the whole kart according to the axis
         if (currentSpeed!=0)
-            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0),
-                    Time.deltaTime * 5f);
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0),
+                    Time.deltaTime * rotateSpeed);
+
+        //Manage the camera fov according to speed
+        CameraController.instance.changeFov(currentSpeed* speedFovModifier);
 
         //Accumulate the drift power
         doDrift();
--- Assets/Scripts/Misc/CameraController.cs
+++ Assets/Scripts/Misc/CameraController.cs
@@ -5,12 +5,30 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class CameraController : MonoBehaviour
 {
+    public Camera cam;
+
     public GameObject followTarget;
     public Vector3 distanceToTarget;
+
+    public float minFov;
+    public float maxFov;
+
+    public static CameraController instance;
+
+    private void Start()
+    {
+        if (instance == null) instance = this;
+    }
 
     private void LateUpdate()
     {
        transform.LookAt(followTarget.transform);
+    }
+
+    public void changeFov(float value)
+    {
+        value = Mathf.Clamp(value, minFov, maxFov);
+        cam.fieldOfView = value;
     }
 
     private void Update()
